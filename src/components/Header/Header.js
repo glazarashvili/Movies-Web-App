f3daeccc88cf52e096c63199c53b83c431331a6c
glazarashvili <giorgi.lazarashvili96@gmail.com>
2021-07-08 11:55:05 +0400

    added slider and header bar

---
+++ src/components/Header/Header.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import tvSvg from '../../assets/svg/tv.svg'
+import arrowSvg from '../../assets/svg/circle.svg'
+import circleSvg from '../../assets/svg/arrow.svg'
+import movieSvg from '../../assets/svg/movie.svg'
+import theaterSvg from '../../assets/svg/theater.svg'
+
+import classes from './Header.module.css'
+
+const Header = () => {
+  return (
+    <div className={classes['header-bar']}>
+      <h1 className={classes.heading}>Cinemania</h1>
+      <nav className={classes['header-navigation']}>
+        <div>
+          <img src={movieSvg} alt='movieSvg' />
+          <p>Movies</p>
+        </div>
+        <div>
+          <img src={tvSvg} alt='tvSvg' />
+          <p>TV</p>
+        </div>
+        <div>
+          <img src={theaterSvg} alt='theaterSvg' />
+          <p>Cinemania</p>
+        </div>
+        <div>
+          <img src={arrowSvg} alt='arrowSvg' />
+          <p>TV Shows</p>
+        </div>
+        <div>
+          <img src={circleSvg} alt='circleSvg' />
+          <p>Player</p>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/MainLayout/MainLayout.js
@@ -0,0 +1,84 @@
+import React from 'react'
+
+import classes from './MainLayout.module.css'
+
+import posters from './index'
+
+const MainLayout = () => {
+  const [posterIndex, setPosterIndex] = React.useState(0)
+
+  let intViewportHeight = window.innerHeight
+
+  const nextButtonClicked = () => {
+    if (posterIndex > posters.length) {
+      console.log('aeeeeee')
+      setPosterIndex(0)
+    } else if (posterIndex >= 0 && posterIndex < posters.length) {
+      setPosterIndex(prevState => prevState + 1)
+    }
+  }
+
+  const prevButtonClicked = () => {
+    if (posterIndex > posters.length) {
+      console.log('aeeeeee')
+      setPosterIndex(0)
+    } else if (posterIndex >= 0 && posterIndex < posters.length) {
+      setPosterIndex(prevState => prevState + 1)
+    }
+  }
+
+  console.log(posterIndex)
+
+  return (
+    <div
+      className={classes['main-layout']}
+      style={{ height: intViewportHeight / 2 }}>
+      <img
+        style={{
+          backgroundSize: 'hover',
+          height: '100%',
+          objectFit: 'cover',
+          width: '100%',
+          backgroundImage: posters[posterIndex].url,
+        }}
+        src={posters[posterIndex].url}
+      />
+      <div
+        style={{
+          backgroundSize: 'hover',
+          height: '100%',
+          objectFit: 'cover',
+          width: '100%',
+          backgroundImage: posters[posterIndex].url,
+        }}></div>
+      <svg
+        className={classes.arrowBack}
+        onClick={prevButtonClicked}
+        width='34'
+        height='172'
+        viewBox='0 0 34 172'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'>
+        <path
+          d='M32.9091 1L2 86L32.9091 171'
+          stroke='#999999'
+          stroke-width='2'></path>
+      </svg>
+      <svg
+        className={classes.arrowForward}
+        onClick={nextButtonClicked}
+        width='34'
+        height='172'
+        viewBox='0 0 34 172'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'>
+        <path
+          d='M1.09091 171L32 86L1.09092 1'
+          stroke='#999999'
+          stroke-width='2'></path>
+      </svg>
+    </div>
+  )
+}
+
+export default MainLayout

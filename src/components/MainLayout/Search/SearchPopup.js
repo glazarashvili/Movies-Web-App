987f747935c4475139f80a25577f73c3553c5138
glazarashvili <giorgi.lazarashvili96@gmail.com>
2021-07-15 16:31:38 +0400

    added some animations to search menu

--- src/components/MainLayout/Search/SearchPopup.js
+++ src/components/MainLayout/Search/SearchPopup.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 import classes from './SearchPopup.module.css'
 
 const SearchPopup = ({ className, popupShown }) => {
+  const overlayRef = useRef(null)
+
+  useEffect(() => {
+    console.log(overlayRef.current, 'current')
+  }, [])
+
   return (
     <React.Fragment>
-      <div className={classes['search-popup']}>
+      <div
+        className={`${classes['search-popup']} ${classes['fade-animation']}`}>
         <img
           src='https://bmscontent1.adjarabet.com/9062/Untitled-13.jpg'
           alt='adv-poster'
         />
       </div>
-      <div onClick={popupShown} className={className}></div>
+      <div
+        onClick={popupShown}
+        className={`${className} ${classes['fade-animation']}`}
+        ref={overlayRef}>
+        adsf
+      </div>
     </React.Fragment>
   )
 }

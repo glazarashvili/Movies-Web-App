6476f4f9a0d15c8e55a3b514d788f937d1efef78
glazarashvili <giorgi.lazarashvili96@gmail.com>
2021-07-21 12:55:06 +0400

    added movie details card container

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -1,30 +1,34 @@
 import React from 'react'
 import axios from 'axios'
+import { useParams } from 'react-router-dom'
+
 import { url, apiKey, posterUrl } from '../../service/index'
 
-import { useParams } from 'react-router-dom'
 import MovieCard from './MovieCard'
 
 // import classes from './MovieDetail.module.css'
 
 export const MovieDetail = () => {
   const params = useParams()
-
-  const [movieDetail, setMovieDetail] = React.useState()
+  const [movieDetail, setMovieDetail] = React.useState({})
 
   const movieUrl = `${url}/movie/${params.movieId.slice(1)}?api_key=${apiKey}`
 
   React.useEffect(() => {
     axios.get(movieUrl).then(response => setMovieDetail(response.data))
-  }, [])
+  }, [movieUrl])
+
+  const poster = posterUrl + movieDetail?.backdrop_path
 
   console.log(movieDetail)
 
-  // const backImage =
-
   return (
     <div style={{ marginTop: '92px' }}>
-      <MovieCard backImage={posterUrl + movieDetail?.backdrop_path} />
+      <MovieCard
+        poster={poster}
+        movieTitle={movieDetail.title}
+        ranking={movieDetail.vote_average}
+      />
     </div>
   )
 }

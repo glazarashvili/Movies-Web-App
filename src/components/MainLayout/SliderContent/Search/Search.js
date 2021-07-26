e1c81b4d4a84a6239122393043870192498331cf
glazarashvili <giorgi.lazarashvili96@gmail.com>
2021-07-26 14:56:23 +0400

    added movie data fetching funciontlity

--- src/components/MainLayout/SliderContent/Search/Search.js
+++ src/components/MainLayout/SliderContent/Search/Search.js
@@ -4,18 +4,36 @@ import SearchPopup from './SearchPopup'
 import SearchInput from './SearchInput'
 import SearchIcon from '../../../SVG/SearchIcon'
 
+import axios from 'axios'
+
+import { url, apiKey } from '../../../../service/index'
+
 import classes from './Search.module.css'
 import { useDelayUnmount } from '../../../../hooks/useDelayUnmount'
 
 const Search = () => {
   const [isMounted, setIsMounted] = React.useState(false)
+  const [searchedMovies, setSearchedMovies] = React.useState([])
   const shouldRenderChild = useDelayUnmount(isMounted, 400)
+
+  const searchMovie = e => {
+    console.log(e.target.value)
+    if (e.target.value.length > 3) {
+      axios
+        .get(`${url}/search/movie?query=${e.target.value}&api_key=${apiKey}`)
+        .then(response => {
+          console.log(response)
+          setSearchedMovies(response.data.results)
+        })
+    }
+  }
 
   return (
     <div className={classes['search-bar']}>
       <SearchInput
         zIndex={isMounted ? 2000 : 0}
         onFocus={() => setIsMounted(true)}
+        searchMovie={searchMovie}
       />
       <SearchIcon
         zIndex={isMounted ? 2000 : 0}
@@ -24,6 +42,7 @@ const Search = () => {
       {shouldRenderChild && (
         <SearchPopup
           className={`${classes.backdrop}`}
+          searchedMovies={searchedMovies}
           animationClass={`${
             isMounted ? classes['fade-animation'] : classes['hide-animation']
           }`}
--- src/components/MainLayout/SliderContent/Search/SearchPopup.js
+++ src/components/MainLayout/SliderContent/Search/SearchPopup.js
@@ -2,17 +2,35 @@ import React from 'react'
 
 import classes from './SearchPopup.module.css'
 
-const SearchPopup = ({ className, popupShown, animationClass }) => {
+import { posterUrl } from '../../../../service/index'
+
+const SearchPopup = ({
+  className,
+  popupShown,
+  animationClass,
+  searchedMovies,
+}) => {
   const popupWidth = window.innerWidth / 2
 
   return (
     <React.Fragment>
       <div className={`${classes['search-popup']} ${animationClass}`}>
-        <img
+        {/* <img
           src='https://bmscontent1.adjarabet.com/9062/Untitled-13.jpg'
           alt='adv-poster'
           style={{ width: popupWidth, border: '5px' }}
-        />
+        /> */}
+        <div style={{ width: popupWidth, border: '5px' }}>
+          {searchedMovies.map(elem => {
+            return (
+              <img
+                className={classes['movie-poster']}
+                src={posterUrl + elem?.poster_path}
+                alt='searched-poster'
+              />
+            )
+          })}
+        </div>
       </div>
       <div
         onClick={popupShown}

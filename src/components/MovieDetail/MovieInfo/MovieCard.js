c98f19182f0b8619690e37abef9af2caeef22f07
glazarashvili <giorgi.lazarashvili96@gmail.com>
2021-07-28 22:37:06 +0400

    added trailer logo player

--- src/components/MovieDetail/MovieInfo/MovieCard.js
+++ src/components/MovieDetail/MovieInfo/MovieCard.js
@@ -2,13 +2,17 @@ import React from 'react'
 
 import classes from './MovieCard.module.css'
 
+// import PlayerSvg from '../../SVG/PlayerSvg'
 import Template from '../../../ui/Template'
 
-const MovieCard = ({ backdrop, movieTitle, ranking }) => {
+const MovieCard = ({ backdrop, movieTitle, ranking, showModal }) => {
   const background = `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.8)), url(${backdrop}) no-repeat center center/cover`
 
   return (
     <div className={classes['movie-card']} style={{ background: background }}>
+      {/* <div className={classes['trailer-logo']} onClick={showModal}>
+        <PlayerSvg />
+      </div> */}
       <Template className={classes['movie-content']}>
         <h1 className={classes['movie-title']}>{movieTitle}</h1>
         <div className={classes['movie-ranking']}>
